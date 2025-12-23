797ff9efbaee1c54dde7cb0c2e44da49a6286648
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 14:51:41 +0900

    메뉴 넣기

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ class cafeMenu:
         def order_output(self):
          print(f"주문하신 {cafe_food} 나왔습니다.")
 
+americano = cafeMenu("아메리카노", 5000)
+cafelatte = cafeMenu("카페라떼", 5500)
+bananalatte = cafeMenu("바닐라라뗴", 6000)
+
+americano.order_output()
+cafelatte.order_output()
