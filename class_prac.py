48fde4f9f9066305a0cecb164a342f6eb7df4c84
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 15:55:02 +0900

    내가 좋아하는 책 입력

--- class_prac.py
+++ class_prac.py
@@ -7,4 +7,6 @@ class book:
         
         return f"이 책의 제목은 {self.book_title}이고, 저자는 {self.book_author}입니다."
     
-
+first_book = book("아프니까 청춘이다.", "김난도")
+second_book = book("나는 내일, 어제의 너와 만난드", "나나츠키 타카후미")
+third_book = book("성경", "하느님")
