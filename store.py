0ec8a1b27a1e07819e477c93a71eef3db3f0c23b
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 16:56:53 +0900

    store -> store_list로 단어변경

--- store.py
+++ store.py
@@ -1,11 +1,11 @@
-store = [
+store_list = [
     {"분류 1":["콜라","사이다","환타"]},
     {"분류 2":["포카칩", "홈런볼"]},
     {"분류 3":["신라면", "불닭볶음면", "진라면"]}
     ]
 
-for i in range(len(store)):
-    store_type = store[i]
+for i in range(len(store_list)):
+    store_type = store_list[i]
     # print(store_type)
 
     for j in store_type:
