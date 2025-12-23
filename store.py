a73ef82f7c785088335e948075cf94ba52552c66
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-23 16:12:20 +0900

    바깥 for문: store의 객체 확인

--- store.py
+++ store.py
@@ -1,5 +1,9 @@
-sotre = [
+store = [
     {"분류 1":["콜라","사이다","환타"]},
     {"분류 2":["포카칩", "홈런볼"]},
     {"분류 3":["신라면", "불닭볶음면", "진라면"]}
     ]
+
+for i in range(len(store)):
+    store_type = store[i]
+    # print(store_type)/
